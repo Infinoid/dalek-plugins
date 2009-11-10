ed70289ed82ad1f20d080850b22cafb09f04e248
Moritz Lenz <mlenz@wvbh074.physik.uni-wuerzburg.de>
2009-11-10 16:35:21 +0100

    enable reporting of github branches, and follow rakudo-ng

---
+++ modules/local/rakudonglog.pm
@@ -0,0 +1,39 @@
+package modules::local::rakudolog;
+use strict;
+use warnings;
+use base 'modules::local::githubparser';
+
+
+=head1 NAME
+
+    modules::local::rakudonglog
+
+=head1 DESCRIPTION
+
+This is a subclass of modules::local::githubparser.  It adds a parser to emit
+rakudo commits on the 'ng' branch.
+
+Normally rakudo commits are configured automatically by
+modules::local::autofeed.  However, that autoconfig sends the messages to
+magnet's #parrot, whereas we want rakudo commits to go to freenode's #perl6 as
+well.
+
+
+=head1 METHODS
+
+=head2 init
+
+This is a pseudo-method called by botnix when the module is first loaded.  It
+hands the URL to the github parser module and tells it to emit to freenode's
+#perl6.  (If a rakudo parser was already configured, it will consolidate the
+config and simply add the freenode channel to the list of targets.)
+
+=cut
+
+my $url = 'http://github.com/rakudo/rakudo';
+
+sub init {
+    modules::local::githubparser->try_link($url, ['freenode', '#perl6', 'ng']);
+}
+
+1;
